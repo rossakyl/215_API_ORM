dcf4ce8d0fb13d3fb9c30726b1c9bb5784f5098b
rossakyl <rossakayla01@gmail.com>
2026-07-20 14:42:15 +0700

    membuat model komik dengan atribut id, title, description, dan author

--- models/komik.js
+++ models/komik.js
@@ -1,3 +1,19 @@
 module.exports = (sequelize, DataTypes) => {
-    
+    const Komik = sequelize.define("Komik", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+        },
+        description: {
+            type: DataTypes.TEXT,
+        },
+        author: {
+            type: DataTypes.STRING,
+        },
+    });
+    return Komik;
 };      
